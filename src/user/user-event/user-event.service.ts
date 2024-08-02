e2c99ff8288e43600a22f4464bb31137ae379531
kavindyakdn <kavindya.kdn@gmail.com>
2024-08-02 21:46:22 +0530

    user event apis

--- src/user/user-event/user-event.service.ts
+++ src/user/user-event/user-event.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserEventService {}
+export class UserEventService {
+  constructor(private prisma: PrismaService) {}
+
+  async getEvent(id: string) {
+    try {
+      const event = await this.prisma.event.findUnique({
+        where: {
+          id: id,
+        },
+        select: {
+          id: true,
+          name: true,
+          dateTime: true,
+          clubId: true,
+        },
+      });
+
+      if (!event) {
+        throw new ForbiddenException('Club not found');
+      }
+
+      return event;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getEvents() {
+    try {
+      const events = await this.prisma.event.findMany({
+        select: {
+          id: true,
+          name: true,
+          dateTime: true,
+          clubId: true,
+        },
+      });
+
+      return events;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
