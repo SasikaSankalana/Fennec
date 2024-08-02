433eb23af47b9ad7ed64158bc75e6716d7bdf7e0
kavindyakdn <kavindya.kdn@gmail.com>
2024-08-02 12:12:06 +0530

    events module removed

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -4,10 +4,9 @@ import { AdminController } from './admin.controller';
 import { AdminClubModule } from './admin-club/admin-club.module';
 import { AdminClubNightModule } from './admin-club-night/admin-club-night.module';
 import { AdminPromotionModule } from './admin-promotion/admin-promotion.module';
-import { AdminEventModule } from './admin-event/admin-event.module';
 
 @Module({
-  imports: [AdminClubModule, AdminClubNightModule, AdminPromotionModule, AdminEventModule],
+  imports: [AdminClubModule, AdminClubNightModule, AdminPromotionModule],
   providers: [AdminService],
   controllers: [AdminController],
 })
